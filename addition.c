db96b613c8687608fb03bb55f8a08149e5a04c06
JeremyRivera <jeremyrivera23@yahoo.com>
2021-01-22 02:24:36 -0500

    Update addition.c

--- addition.c
+++ addition.c
@@ -11,3 +11,14 @@ int main(void)
     
     printf("%li\n", x + y);
 }
+//another example
+int main(void)
+{
+    // get numbers from user
+    int x = get_int("x: ");
+
+     int y = get_int("y: ");
+
+     float z = (float) x / (float) y;
+     printf("%f\n", z);
+}
